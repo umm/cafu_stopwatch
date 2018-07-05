721e846f11ab8f74ae532191fbb5e7c6ead06432
mattak <mattak.me@gmail.com>
2018-07-06 01:41:53 +0900

    feat/#1 :gem: Test StopwatchUseCase.StartedAsObservable

---
+++ Assets/Tests/EditMode/Domain/UseCase/StopwatchUseCaseTest.cs
@@ -0,0 +1,49 @@
+﻿using ExtraUniRx;
+using NUnit.Framework;
+using UnityModule;
+using UniRx;
+
+namespace CAFU.Stopwatch.Domain.UseCase
+{
+    public class StopwatchUseCaseTest
+    {
+        class StopwatchUseCaseSpy : StopwatchUseCase
+        {
+            public void Initialize(ISubject<float> frameDiffTime)
+            {
+                base.Initialize();
+                this.Stopwatch = new StopWatch(frameDiffTime);
+            }
+        }
+
+        private StopwatchUseCaseSpy usecase;
+        private ISubject<float> frameDiffTime;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.frameDiffTime = new Subject<float>();
+            this.usecase = new StopwatchUseCaseSpy();
+            this.usecase.Initialize(this.frameDiffTime);
+        }
+
+        [Test]
+        public void StartedAsObservableTest()
+        {
+            Assert.IsFalse(this.usecase.IsPlaying);
+
+            var observer = new TestObserver<Unit>();
+            this.usecase.StartedAsObservable.Subscribe(observer);
+            Assert.AreEqual(0, observer.OnNextCount);
+
+            this.usecase.Start();
+            Assert.AreEqual(1, observer.OnNextCount);
+
+            this.usecase.Stop();
+            Assert.AreEqual(1, observer.OnNextCount);
+
+            this.usecase.Start();
+            Assert.AreEqual(2, observer.OnNextCount);
+        }
+    }
+}
